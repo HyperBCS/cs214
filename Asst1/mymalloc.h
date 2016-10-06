b1e8c96f5fef8b05337305571fa933504870b245
BuildTools <unconfigured@null.spigotmc.org>
2016-10-05 20:23:22 -0400

    init asst1

---
+++ Asst1/mymalloc.h
@@ -0,0 +1,14 @@
+#ifndef _mymalloc_h_
+#define _mymalloc_h_
+
+#include <stdlib.h>
+#include <stdio.h>
+#define malloc( x ) mymalloc( x, __FILE__, __LINE__ )
+#define foo( x ) foo2( x, __FILE__, __LINE__ )
+//#define free( x ) myfree( x, __FILE__, __LINE__ )
+
+int foo2(int i);
+
+void *mymalloc(int size, char* file, int line);
+
+#endif
