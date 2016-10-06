20df7a160cee5723f36ea475d66fc9879ec1860f
Asad Dar <asad7dar@gmail.com>
2016-10-05 21:09:07 -0400

    hi

--- Asst1/mymalloc.c
+++ Asst1/mymalloc.c
@@ -2,11 +2,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+static char total_memory[5000];
+
+typedef struct memory_block {
+	int size;
+	int allocated;
+	int address;
+	struct memory_block *next;
+} mem_block;
+
 int foo2(int i){
 	i = i*i;
 	return i;
 }
 
 void *mymalloc(int size, char* file, int line){
-	return 0;
+	static mem_block block = (mem_block*) total_memory; 
+	return *total_memory;
 }
