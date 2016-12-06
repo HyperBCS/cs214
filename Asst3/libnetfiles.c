1ba2712c29a3c5005da29e82daf8df20d6ab7bd3
BuildTools <atomicbcs@gmail.com>
2016-12-06 00:45:16 -0500

    Some stuff

--- Asst3/libnetfiles.c
+++ Asst3/libnetfiles.c
@@ -74,7 +74,7 @@ int netopen(const char * pathname,int flags){
 	// send flag
 	int count_flags = send(sockfd, &flags, sizeof(flags), 0);
 	// send path
-	int countF = send(sockfd, filename, sizeof(pathname), 0);
+	int countF = send(sockfd, filename, 256, 0);
 	int retnB = recv(sockfd, &error, sizeof(error), 0);
 	int retnE = recv(sockfd, &err, sizeof(err), 0);
 	if(error == -1){
@@ -82,17 +82,85 @@ int netopen(const char * pathname,int flags){
 		printf("[%d] %s\n",error,strerror(errno));
 	}
 	close(sockfd);
-	return retnB;
+	return error;
+}
+
+ssize_t netread(int fildes, void *buf, size_t nbyte){
+	// remember to set errno
+	if(fildes >=0){
+		return -1;
+	}
+	int sockfd;
+	char ip[100];
+	int err;
+	int t = 2;
+	// init server addr and client addr
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if(sockfd < 0){
+		printf("ERROR: Could not open socket, please check connection!\n");
+		exit(0);
+	}
+	socklen_t addrLen = sizeof(serv_addr);
+	inet_ntop(AF_INET, &serv_addr.sin_addr, ip, sizeof ip);
+	printf("Connecting to %s\n",ip);
+	int status = connect(sockfd, (struct sockaddr *) &serv_addr, addrLen);
+	if(status < 0){
+		printf("Could not connect to server.\n");
+		exit(1);
+	}
+	// send type
+	int type = send(sockfd, &t, sizeof(type), 0);
+	// send fdes
+	int count_fdes = send(sockfd, &fildes, sizeof(fildes), 0);
+	//send nbytes
+	int count_nbyte = send(sockfd, &nbyte, sizeof(nbyte), 0);
+	//receive error first
+	int retnE = recv(sockfd, &err, sizeof(err), 0);
+	if(retnE != sizeof(int)){
+		printf("Could not receive error!\n");
+	}
+	if(err != 0){
+		errno = err;
+		printf("[%d] %s\n",err,strerror(errno));
+		close(sockfd);
+		return -1;
+	}
+	printf("ERR: %d\n",err);
+	int retnB = 0;
+	while(retnB < nbyte){
+		int tmp;
+		tmp = recv(sockfd, buf, nbyte, 0);
+		buf += tmp;
+		retnB += tmp;
+		printf("Received: %d/%zd\n",retnB,nbyte);
+	}
+	close(sockfd);
+	return err;
 }
 
 int main(int argc, char ** argv){
-	if(argc != 3){
-		printf("Incorrect arguments\n");
-		exit(1);
-	}
-	char * hostname = argv[1];
+	// if(argc != 3){
+	// 	printf("Incorrect arguments\n");
+	// 	exit(1);
+	// }
+	// char * hostname = argv[1];
+	char * hostname = "localhost";
 	netserverinit(hostname,1);
-	netopen(argv[2],O_RDONLY);
+	char * filename = "f.txt";
+	int fdd = netopen(filename,O_RDONLY);
+	 //FILE * file = fopen(filename, "r");
+	 //fseek(file, 0L, SEEK_END);
+	 //int size = ftell(file);
+	// printf("SIZE: %d\n",size);
+	// //printf("FD: %d\n",fdd);
+	// sleep(1);
+	 char * txt = calloc(1,100);
+	netread(fdd,txt,1000000);
+	// char * file_n = "music2.mp3";
+ //    FILE * fp = fopen (file_n, "w+");
+ //    fwrite(txt, sizeof(char), size, fp);
+    //fclose(fp);
+  //  fclose(file);
 }
 
 
--- Asst3/netfileserver.c
+++ Asst3/netfileserver.c
@@ -15,15 +15,16 @@
 
 extern int errno;
 
-struct sockets {
-	struct sockets * next;
+struct fdl {
+	struct fdl * next;
     char * filename;
     int mode;
+    int fd;
     int file_mode;
 }arg;
 
 // cant make a linked list
-struct sockets of = calloc(1,sizeof(arg));
+struct fdl * of;
 
 void error(char *msg){
   perror(msg);
@@ -31,49 +32,75 @@ void error(char *msg){
 }
 
 //mutex lock this
-int conflict(char * filename, int mode, int readtype){
+int * conflict(char * filename, int mode, int readtype){
 	int i;
-	int ok;
-	struct * fds = of;
-	struct * possible = 0;
+	int mode0;
+	struct fdl * fds = of;
+	struct fdl * possible = 0;
+	int mode1 = 0;
+	int write1 = 0;
 	for(;fds != 0;fds = fds->next){
 		if(fds->next == 0){
 			possible = fds;
 		}
-		if(strcmp(fds->filename,filename) && mode == 0){
-			ok = 1;
-			break;
+		if(!strcmp(fds->filename,filename) && fds->mode == 0){
+			write1 = 1;
+			mode0 = 1;
+			continue;
 		}
-		if(strcmp(fds->filename,filename) && mode == 1 && readtype == O_RDONLY){
-			ok = 1;
-			break;
+		if(!strcmp(fds->filename,filename) && fds->mode == 1 && fds->file_mode == O_RDONLY){
+			mode1 = 1;
+			continue;
 		}
-		if(strcmp(fds->filename,filename) && mode == 1 && fds->file_mode != O_RDONLY && readtype == O_RDONLY){
-			ok = 1;
-			break;
+		if(!strcmp(fds->filename,filename) && fds->mode == 1 && fds->file_mode != O_RDONLY){
+			write1 = 1;
+			mode1 = 1;
+			continue;
 		}
-		if(strcmp(fds->filename,filename) && mode == 2){
-			return -1;
+
+		if(!strcmp(fds->filename,filename) && fds->mode == 2){
+			return 0x0;
 		}
 	}
-	if(ok){
-		possible->next = calloc(1,sizeof(arg));
-		possible = possible->next;
+	if((mode1 || mode0) && mode == 2){
+		return 0x0;
+	}
+	fds = possible;
+	if((mode1 == 0) || (mode1 == 1 && write1 == 1 && readtype == O_RDONLY) || (mode1 == 1 && write1 == 0) || fds == 0){
+		if(fds == 0){
+			of = calloc(1,sizeof(arg));
+			possible = of;
+		} else{
+			possible->next = calloc(1,sizeof(arg));
+			possible = possible->next;
+		}
 		possible->filename = filename;
 		possible->mode = mode;
 		possible->file_mode = readtype;
-		return possible
+		return &possible->fd;
 	}
-	return -1;
+	return 0x0;
 	
 }
 
+void listfiles(){
+	struct fdl * fds = of;
+	printf("-------FILES-------\n");
+	for(;fds != NULL;fds = fds->next){
+		printf("FILE: %s\nMODE: %d\nFD: %d\nFILE MODE: %d\n\n",fds->filename,fds->mode,fds->fd,fds->file_mode);
+	}
+	printf("-------------------\n");
+}
+
 int netopen(int client){
-	char recvBuf[256];
+	char * recvBuf = calloc(1,256);
 	int flagBuf;
 	int mode;
 	int error;
+	int omode;
 	int i;
+	int * bad;
+	int o;
 	// receive connection mode (exclusive, transaction, etc)
 	int count_mode = recv(client, &mode, sizeof(int), 0);
 	// receive flags(read only,write,wr)
@@ -81,14 +108,67 @@ int netopen(int client){
 	//path name
 	int count_f = recv(client, recvBuf, 256, 0);
 	//check for modes
-	conflict(recvBuf,mode,flagBuf);
-	int o = open(recvBuf,flagBuf);
-	if(o == -1){
-		error = errno;
+	o = open(recvBuf,flagBuf);
+	if(o != -1){
+		bad = conflict(recvBuf,mode,flagBuf);
+	}
+	if(bad == 0){
+		printf("Conflict\n");
+		// permision denied fix
+		error= 1;
+		o = -1;
+	} else if(bad){
+		if(o == -1){
+			error = errno;
+		} else{
+			o *= -10;
+			*bad = o;
+		}
 	}
 	printf("Flag: %d\nMode: %d\nFilename: %s\n",flagBuf,mode,recvBuf);
 	int retnB = send(client, &o, sizeof(o), 0);
 	int retnE = send(client, &error, sizeof(error), 0);
+	listfiles();
+	return o;
+}
+
+int netread(int client){
+	char * readBuf;
+	int filedes;
+	int error;
+	ssize_t nbytes;
+	int o;
+	int re;
+	// receive file descriptor
+	int count_fdes = recv(client, &filedes, sizeof(int), 0);
+	// receive # of bytes to read
+	int count_nbyte = recv(client, &nbytes, sizeof(ssize_t), 0);
+	//printf("FD: %d\nNBYTE: %zd\n",filedes,nbytes);
+	readBuf = calloc(1,nbytes);
+	if(filedes % 10 == 0){
+		filedes /= -10;
+		re = read(filedes,readBuf,nbytes);
+		printf("RE: %d\n",re);
+		printf("Write Size: %zd\n",nbytes);
+	} else{
+		re = -1;
+	}
+	if(re == -1){
+		error = errno;
+		if(error == 0){
+			error = -1;
+		}
+	} else{
+		error = 0;
+	}
+	int retnE = send(client, &error, sizeof(error), 0);
+	if(re == -1){
+		free(readBuf);
+		return -1;
+	}
+	int retnB = send(client, readBuf, nbytes, 0);
+	printf("Sent: %d\n",retnB);
+	free(readBuf);
 	return o;
 }
 
@@ -103,6 +183,7 @@ void * begin(void * c){
 			netopen(client);
 			break;
 		case 2:
+			netread(client);
 			break;
 		case 3:
 			break;
